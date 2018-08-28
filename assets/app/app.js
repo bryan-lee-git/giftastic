c243ea765355e1adf37a4f315c910a166ed9216c
bryanlee <blee@utahsbr.edu>
2018-08-28 15:42:29 -0600

    updated styles for theater view, updated site background, tried more solutions for download on click of button

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,16 +1,20 @@
-// GIPHY API Gif Search Web
+
+// GIFtastic - GIPHY API Gif Search / View / Download App
+
+//-----------------------------------------------------------------------------------------------
+// GLOBAL VARIABLES
+//-----------------------------------------------------------------------------------------------
 
 // initial array of gifs
-var gifs = ["Debugging", "Coding", "Web Dev", "Punching Computer", "Coding Frustration"];
+var gifs = ["Debugging Code", "Coding", "Web Dev", "Punch Computer", "Coding Frustration", "Coding Meme", "Mind Blown", "Picard", "Nerd Life", "The Borg"];
 
-// starting and eding indexes for iterating through GIPHY API Response Data
+// starting and ending indexes for iterating through GIPHY API Response Data
 var startingIndex = 1;
 var endingIndex = 5;
 
-// prevent default submit button action
-$("#gif-form").submit(function(e) {
-  e.preventDefault();
-})
+//-----------------------------------------------------------------------------------------------
+// APP FUNCTIONS
+//-----------------------------------------------------------------------------------------------
 
 // function for displaying array data in buttons
 function renderButtons() {
@@ -23,108 +27,6 @@ function renderButtons() {
     $("#gifs-btns").append($button);
   }
 }
-
-// function to handle events where one button is clicked
-$("#add-gif").on("click", function() {
-
-  // assign user input value to a variable
-  var gifData = $("#gif-input").val();
-
-  // If nothing was input by the user before submit, alert an error
-  if (gifData === "") {
-    alert("Invalid Input. Try Again");
-  } 
-
-  // if there is data, add it to a button in the button area and store the value for later API use
-  else {
-    gifs.push(gifData);
-    var newGifBtn = $("<button class='gif-button gif btn'>" + gifData + "</button>");
-    $("#gifs-btns").append(newGifBtn);
-    newGifBtn.attr("data-gifTopic", gifData);
-    newGifBtn.hide(0);
-    newGifBtn.fadeIn(500);
-  }
-})
-
-// GIF topic button functionality
-$(document).on("click", ".gif", function() {
-
-  // reset starting and ending indexes for new topic
-  startingIndex = 1;
-  endingIndex = 5;
-
-  // empty out the gif and instructions display
-  $("#gifs-view").empty();
-
-  // hide the footer so that it does not overlap content
-  $(".footer").fadeOut(200);
-
-  // create variable to hold the data value we will run through the API call function
-  var gif = $(this).attr('data-gifTopic');
-
-  // empty out topic display area
-  $("#topic-div").empty();
-
-  // add new topic to topic display area
-  $("#topic-div").prepend("Selected Topic: " + gif);
-  $("#topic-div").hide(0);
-  $("#topic-div").fadeIn(1000);
-
-  // create and display instructions
-  $("#instructions-div").empty();
-  var instructions = $("<h3 id='instructions'>" + "Click to View Animation / Reclick to Stop Animation / Dbl Click to Download" + "</h3>" + "<br>");
-  $("#instructions-div").append(instructions);
-  instructions.hide(0);
-  instructions.fadeIn(1000);
-
-  // console log the object to make sure it is being assigned to the variable correctly
-  console.log(gif);
-
-  // run the GIF API Call function using the gif variable
-  getGifData(gif);
-
-  // fade the new gif data in
-  $("#gifs-view").fadeIn(500);
-
-  // previous and next buttons
-  $("#buttons-view").empty();
-
-  // button for previous
-  var prevBtn = $("<button id='prev-button' class='col-sm-5 col-xs-12 col-md-5 col-lg-5 btn btn-success'><- Previous</button>")
-  $("#buttons-view").append(prevBtn);
-  prevBtn.hide(0);
-  prevBtn.fadeIn(1000);
-
-  // previous button functionality 
-  $("#prev-button").on("click", function() {
-    if (endingIndex > 5) {
-      $("#gifs-view").empty();
-      startingIndex -= 4;
-      endingIndex -= 4;
-      getGifData(gif);
-    }
-  })
-
-  // space between previous and next buttons
-  var btnSpace = $("<div class='col-sm-2 col-xs-0 col-md-2 col-lg-2'></div>")
-  $("#buttons-view").append(btnSpace);
-
-  // button for next
-  var nextBtn = $("<button id='next-button' class='col-sm-5 col-xs-12 col-md-5 col-lg-5 btn btn-success'>Next -></button>")
-  $("#buttons-view").append(nextBtn);
-  nextBtn.hide(0);
-  nextBtn.fadeIn(1000);
-
-  // next button functionality
-  $("#next-button").on("click", function() {
-    if (startingIndex < 21) {
-      $("#gifs-view").empty();
-      startingIndex += 4;
-      endingIndex += 4;
-      getGifData(gif);
-    }
-  })
-})
 
 // function for populating GIF stills on search
 function getGifData(gifTopic) {
@@ -149,6 +51,7 @@ function getGifData(gifTopic) {
       var gif = $("<img class='gif-still' width='100%' height='200px'></img>");
       gif.attr("src", gifData.data[i].images.original_still.url);
       gif.attr("data-altSrc", gifData.data[i].images.original.url);
+      $("#gifs-view").fadeIn(500);
       $("#gifs-view").append(gifDiv);
       gifDiv.append(gif);
       gifDiv.append("<p class='rating'>" + "Rated: " + gifData.data[i].rating + "</p>");
@@ -202,19 +105,145 @@ function getGifData(gifTopic) {
       })
 
       // include a download button for the user to download the gif they are viewing in theater mode
-      var downloadBtn = $("<a target='_blank' href='" + $(this).attr("src") + "' download='" + $(this).attr("src") + "'><button id='download-button' class='col-sm-12 col-xs-12 col-md-12 col-lg-12 btn btn-success'>Download from GIPHY</button></a>");
+      var downloadBtn = $("<button id='download-button' class='col-sm-12 col-xs-12 col-md-12 col-lg-12 btn btn-success'>Download from GIPHY</button></a>");
+      downloadBtn.attr("href", $(this).attr("src"));
+      downloadBtn.attr("download", $(this).attr("src"));
       theaterContainer.append(downloadBtn);
+
+      downloadBtn.on("click", function(e) {
+        e.preventDefault();
+        window.location.href = downloadBtn.attr("href");
+      })
     })
   })
-};
+}
 
+//-----------------------------------------------------------------------------------------------
+// RUN PROGRAM
+//-----------------------------------------------------------------------------------------------
+
+// once page is loaded, do this...
 $(document).ready(function() {
-// app intro slideDown
-$(".jumbotron").hide();
-$(".jumbotron").slideDown(500);
 
-// calling the renderButtons function to display the initial list of gifs
-renderButtons();
+  // app intro slideDown
+  $(".jumbotron").hide();
+  $("#gifs-view").hide();
+  $(".jumbotron").slideDown(500);
+
+  // calling the renderButtons function to display the initial list of gifs
+  renderButtons();
+
+})
+
+//-----------------------------------------------------------------------------------------------
+// CLICK FUNCTIONS
+//-----------------------------------------------------------------------------------------------
+
+// prevent default form submit button action
+$("#gif-form").submit(function(e) {
+  e.preventDefault();
+})
+
+// function to handle events where one topic button is clicked
+$("#add-gif").on("click", function() {
+
+  // assign user input value to a variable
+  var gifData = $("#gif-input").val();
+
+  // If nothing was input by the user before submit, alert an error
+  if (gifData === "") {
+    alert("Invalid Input. Try Again");
+  } 
+
+  // if there is data, add it to a button in the button area and store the value for later API use
+  else {
+    gifs.push(gifData);
+    var newGifBtn = $("<button class='gif-button gif btn'>" + gifData + "</button>");
+    $("#gifs-btns").append(newGifBtn);
+    newGifBtn.attr("data-gifTopic", gifData);
+    newGifBtn.hide(0);
+    newGifBtn.fadeIn(500);
+  }
+})
+
+// GIF topic button functionality
+$(document).on("click", ".gif", function() {
+
+  // reset starting and ending indexes for new topic
+  startingIndex = 1;
+  endingIndex = 5;
+
+  // empty out the gif and instructions display
+  $("#gifs-view").empty();
+
+  // hide the footer so that it does not overlap content
+  $(".footer").fadeOut(200);
+
+  // create variable to hold the data value we will run through the API call function
+  var gif = $(this).attr('data-gifTopic');
+
+  // empty out topic display area
+  $("#topic-div").empty();
+
+  // add new topic to topic display area
+  $("#topic-div").prepend("Selected Topic: " + gif);
+  $("#topic-div").hide(0);
+  $("#topic-div").fadeIn(1000);
+
+  // create and display instructions
+  $("#instructions-div").empty();
+  var instructions = $("<h3 id='instructions'>" + "Click to View Animation | Reclick to Stop Animation | Dbl Click for Theater View" + "</h3>" + "<br>");
+  $("#instructions-div").append(instructions);
+  instructions.hide(0);
+  instructions.fadeIn(1000);
+
+  // console log the object to make sure it is being assigned to the variable correctly
+  console.log(gif);
+
+  // run the GIF API Call function using the gif variable
+  getGifData(gif);
+
+  // fade the new gif data in
+  $("#gifs-view").fadeIn(500);
+
+  // previous and next buttons
+  $("#buttons-view").empty();
+
+  // button for previous
+  var prevBtn = $("<button id='prev-button' class='col-sm-5 col-xs-12 col-md-5 col-lg-5 btn btn-success'><- Previous</button>")
+  $("#buttons-view").append(prevBtn);
+  prevBtn.hide(0);
+  prevBtn.fadeIn(1000);
+
+  // previous button functionality 
+  $("#prev-button").on("click", function() {
+    if (endingIndex > 5) {
+      $("#gifs-view").empty();
+      startingIndex -= 4;
+      endingIndex -= 4;
+      getGifData(gif);
+    }
+  })
+
+  // space between previous and next buttons
+  var btnSpace = $("<div class='col-sm-2 col-xs-0 col-md-2 col-lg-2'></div>")
+  $("#buttons-view").append(btnSpace);
+
+  // button for next
+  var nextBtn = $("<button id='next-button' class='col-sm-5 col-xs-12 col-md-5 col-lg-5 btn btn-success'>Next -></button>")
+  $("#buttons-view").append(nextBtn);
+  nextBtn.hide(0);
+  nextBtn.fadeIn(1000);
+
+  // next button functionality
+  $("#next-button").on("click", function() {
+    if (startingIndex < 21) {
+      $("#gifs-view").empty();
+      startingIndex += 4;
+      endingIndex += 4;
+      getGifData(gif);
+    }
+  })
 })
 
 
