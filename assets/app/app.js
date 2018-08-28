a002b26ef4c142cf94dcafa48e85c295556602b4
bryanlee <blee@utahsbr.edu>
2018-08-28 00:36:52 -0600

    fixed a few functionality bugs for cycling through pages of gifs, updated styles.

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,3 +1,5 @@
+// GIPHY API Gif Search Web
+
 // Initial array of gifs
 var gifs = ["JavaScript", "Debugging", "Coding", "Development", "Cant Get Code to Work", "Coder", "Console.log"];
 var startingIndex = 1;
@@ -11,10 +13,9 @@ $("#gif-form").submit(function(e) {
 // Function for displaying array data in buttons
 function renderButtons() {
   $("#gifs-view").empty();
-
   for (let i = 0; i < gifs.length; i++) {
     var $button = $("<button class='gif-button'>");
-    $button.addClass("gif btn btn-success");
+    $button.addClass("gif btn");
     $button.attr("data-gifTopic", gifs[i]);
     $button.text(gifs[i]);
     $("#gifs-btns").append($button);
@@ -29,7 +30,7 @@ $("#add-gif").on("click", function() {
   } 
   else {
     gifs.push(gifData);
-    var newGifBtn = $("<button class='gif-button gif btn btn-success'>" + gifData + "</button>");
+    var newGifBtn = $("<button class='gif-button gif btn'>" + gifData + "</button>");
     $("#gifs-btns").append(newGifBtn);
     newGifBtn.attr("data-gifTopic", gifData);
   }
@@ -44,7 +45,7 @@ $(document).on("click", ".gif", function() {
   $("#gifs-view").empty();
   $("#instructions-div").empty();
   $(".footer").fadeOut(200);
-  gif = $(this).attr('data-gifTopic');
+  var gif = $(this).attr('data-gifTopic');
   console.log(gif);
   getGifData(gif);
   $("#gifs-view").fadeIn(400);
@@ -59,7 +60,7 @@ function getGifData(gifTopic) {
   instructions.hide(0);
   instructions.fadeIn(1000);
 
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifTopic + "&api_key=dc6zaTOxFJmzC&limit=100";
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifTopic + "&api_key=dc6zaTOxFJmzC";
 
   $.ajax({
     url: queryURL,
@@ -69,7 +70,6 @@ function getGifData(gifTopic) {
     console.log(gifData);
 
     for (let i = startingIndex; i < endingIndex; i++) {
-
       var gifDiv = $("<div class='gif-div col-lg-3 col-sm-6 col-md-3 col-xs-12' style='margin-bottom:10px;padding:20px;'></div>");
       var gif = $("<img class='gif-still' width='100%' height='200px'></img>");
       gif.attr("src", gifData.data[i].images.fixed_width_still.url);
@@ -81,44 +81,44 @@ function getGifData(gifTopic) {
       gif.fadeIn(1000);
     }
 
-    var nextBtn = $("<button id='next-button' class='col-sm-12 btn btn-success'>Next 4</button>")
-    $("#gifs-view").append(nextBtn);
-    nextBtn.hide(0);
-    nextBtn.fadeIn(1000);
+    var prevBtn = $("<button id='prev-button' class='col-sm-5 col-xs-12 col-md-5 col-lg-5 btn btn-success'><--- Previous 4</button>")
+    $("#gifs-view").append(prevBtn);
+    prevBtn.hide(0);
+    prevBtn.fadeIn(1000);
 
-    $("#next-button").on("click", function() {
-      $("#gifs-view").empty();
-      $("#instructions-div").empty();
-      startingIndex += 4;
-      endingIndex += 4;
-      getGifData(gifTopic);
-    })
-
-    if (endingIndex > 5) {
-
-      var prevBtn = $("<button id='prev-button' class='col-sm-12 btn btn-success'>Previous 4</button>")
-      $("#gifs-view").append(prevBtn);
-      prevBtn.hide(0);
-      prevBtn.fadeIn(1000);
-
-      $("#prev-button").on("click", function() {
+    $("#prev-button").on("click", function() {
+      if (endingIndex > 5) {
         $("#gifs-view").empty();
         $("#instructions-div").empty();
         startingIndex -= 4;
         endingIndex -= 4;
         getGifData(gifTopic);
-      })
-    }
+      }
+    })
+
+    var btnSpace = $("<div class='col-sm-2 col-xs-0 col-md-2 col-lg-2'></div>")
+    $("#gifs-view").append(btnSpace);
+
+    var nextBtn = $("<button id='next-button' class='col-sm-5 col-xs-12 col-md-5 col-lg-5 btn btn-success'>Next 4 ---></button>")
+    $("#gifs-view").append(nextBtn);
+    nextBtn.hide(0);
+    nextBtn.fadeIn(1000);
+
+    $("#next-button").on("click", function() {
+      if (startingIndex < 21) {
+        $("#gifs-view").empty();
+        $("#instructions-div").empty();
+        startingIndex += 4;
+        endingIndex += 4;
+        getGifData(gifTopic);
+      }
+    })
 
     $(".gif-still").on("click", function() {
       var temp = $(this).attr("src");
       $(this).attr("src", $(this).attr("data-altSrc"));
       $(this).attr("data-altSrc", temp);
     })
-
   })
 };
-
-//  if GIF is clicked, it switches the src url to the animated version
-
 
