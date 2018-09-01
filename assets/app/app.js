26b98697e717b01c88f9e58030c26efe4d7d7d6b
bryanlee <blee@utahsbr.edu>
2018-09-01 17:47:05 -0600

    added all gif favorite storage functionality, updated site styles, ready to submit

--- assets/app/app.js
+++ assets/app/app.js
@@ -5,17 +5,32 @@
 // GLOBAL VARIABLES
 //-----------------------------------------------------------------------------------------------
 
-// initial array of gifs
 var gifs = ["Debugging Code", "Coder", "Punching Computer", "Coding Frustration", "Mind Blown", "Star Trek", "Picard", "Janeway", "Benjamin Sisko", "The Borg"];
-
-// starting and ending indexes for iterating through GIPHY API Response Data
 var startingIndex = 1;
 var endingIndex = 5;
+var favList = "none";
 
 //-----------------------------------------------------------------------------------------------
 // APP FUNCTIONS
 //-----------------------------------------------------------------------------------------------
 
+// on page load, check if there is anything in storage, load and display stored w/ view favs btn
+// if nothing in storage, set empty array
+function loadFavs() {
+
+  var favParse = JSON.parse(localStorage.getItem("favs"));
+
+  if (favParse.length > 0) {
+    favList = favParse;
+  } else {
+    favList = [];
+  }
+
+  if (favList.length != 0) {
+    var favBtn = $("<button class='btn' id='fav-button'>View Favorites</button>")
+    $("#gifs-btns").prepend(favBtn);
+  }
+}
 // function for displaying array data in buttons
 function renderButtons() {
   $("#gifs-view").empty();
@@ -27,7 +42,6 @@ function renderButtons() {
     $("#gifs-btns").append($button);
   }
 }
-
 // function for populating GIF stills on search
 function getGifData(gifTopic) {
   
@@ -51,16 +65,28 @@ function getGifData(gifTopic) {
       var gif = $("<img class='gif-still' width='100%' height='200px'></img>");
       gif.attr("src", gifData.data[i].images.original_still.url);
       gif.attr("data-altSrc", gifData.data[i].images.original.url);
+      gif.attr("data-download", gifData.data[i].images.original.url);
+      gif.tooltip({boundary: 'window'});
       $("#gifs-view").fadeIn(500);
       $("#gifs-view").append(gifDiv);
       gifDiv.append(gif);
+      gifDiv.prepend("<p style='min-height:25px;' class='title'>" + gifData.data[i].title + "</p>");
       gifDiv.append("<p class='rating'>" + "Rated: " + gifData.data[i].rating + "</p>");
-      gif.hide(0);
-      gif.fadeIn(1000);
+      gifDiv.hide(0);
+      gifDiv.fadeIn(1000);
     }
 
     // switch the src url to the previously called and stored animated gif URL, create a temp for reverse functionality (switches back to static when clicked again)
     $(".gif-still").on("mouseover", function() {
+      // set temp variable to hold still img src
+      var temp = $(this).attr("src");
+      // change img src to alternate animated value stored during API call
+      $(this).attr("src", $(this).attr("data-altSrc"));
+      // replace the alternate animated value with the stored static value to switch and stop animation on next click
+      $(this).attr("data-altSrc", temp);
+    })
+
+    $(".gif-still").on("mouseleave", function() {
       // set temp variable to hold still img src
       var temp = $(this).attr("src");
       // change img src to alternate animated value stored during API call
@@ -83,17 +109,15 @@ function getGifData(gifTopic) {
     $(".gif-still").dblclick(function() {
 
       // run same actions for single click
-      var temp = $(this).attr("src");
-      $(this).attr("src", $(this).attr("data-altSrc"));
-      $(this).attr("data-altSrc", temp);
+      $(this).attr("src", $(this).attr("data-download"));
 
       // make sure we are working with the "this" we want to be working with"
       console.log(this);
 
       // fade main container out, generate and display theater container
       $("#container-main").fadeOut(500);
-      var theaterContainer = $("<div class='container'></div>")
-      var theaterView = $("<div id='theater' class='jumbotron'></div>")
+      var theaterContainer = $("<div class='container'></div>");
+      var theaterView = $("<div id='theater' style='background-color:transparent; padding: none;' class='jumbotron'></div>");
       $("body").append(theaterContainer);
       theaterContainer.append(theaterView);
       theaterContainer.hide(0);
@@ -104,6 +128,7 @@ function getGifData(gifTopic) {
       theaterView.append(this)
 
       // include a back button to return to previous main container view
+      $("#back-button").remove();
       var backBtn = $("<button id='back-button' class='col-sm-12 col-xs-12 col-md-12 col-lg-12 btn btn-success'>Back to Search View</button>");
       theaterContainer.append(backBtn);
 
@@ -113,17 +138,21 @@ function getGifData(gifTopic) {
         getGifData(gifTopic);
         $("#container-main").fadeIn(1000);
       })
+    })
 
-      // include a download button for the user to download the gif they are viewing in theater mode
-      var downloadBtn = $("<button id='download-button' class='col-sm-12 col-xs-12 col-md-12 col-lg-12 btn btn-success'>Download from GIPHY</button></a>");
-      downloadBtn.attr("href", $(this).attr("src"));
-      downloadBtn.attr("download", $(this).attr("src"));
-      theaterContainer.append(downloadBtn);
+    // saving and displaying favorited GIFs    
+    $(".gif-still").on("click", function() {
+      // when gif is clicked, add it to local storage
+      var storeUrl = $(this).attr("data-download");
+      favList.push(storeUrl);
+      localStorage.setItem("favs", JSON.stringify(favList));
+      console.log(favList);
 
-      downloadBtn.on("click", function(e) {
-        e.preventDefault();
-        window.location.href = downloadBtn.attr("href");
-      })
+      if (favList.length > 0) {
+        $("#fav-button").hide();
+        var favBtn = $("<button class='btn' id='fav-button'>View Favorites</button>")
+        $("#gifs-btns").prepend(favBtn);
+      }
     })
   })
 }
@@ -140,6 +169,9 @@ $(document).ready(function() {
   $("#gifs-view").hide();
   $(".jumbotron").slideDown(500);
 
+  //load in any saved data
+  loadFavs();
+
   // calling the renderButtons function to display the initial list of gifs
   renderButtons();
 
@@ -153,8 +185,7 @@ $(document).ready(function() {
 $("#gif-form").submit(function(e) {
   e.preventDefault();
 })
-
-// function to handle events where one topic button is clicked
+// adds a gif topic button with the user's input into the button area
 $("#add-gif").on("click", function() {
 
   // assign user input value to a variable
@@ -175,16 +206,16 @@ $("#add-gif").on("click", function() {
     newGifBtn.fadeIn(500);
   }
 })
-
-// GIF topic button functionality
+// GIF topic button - functionality to load in gifs for topic button clicked
 $(document).on("click", ".gif", function() {
 
-  // reset starting and ending indexes for new topic
+  // reset starting and ending indexes for new topic to start on page 1 of data
   startingIndex = 1;
   endingIndex = 5;
 
   // empty out the gif and instructions display
   $("#gifs-view").empty();
+  $("#title-view").empty();
 
   // hide the footer so that it does not overlap content
   $(".footer").fadeOut(200);
@@ -202,7 +233,7 @@ $(document).on("click", ".gif", function() {
 
   // create and display instructions
   $("#instructions-div").empty();
-  var instructions = $("<h3 id='instructions'>" + "Mouseover/Touch: View or Stop Animation | Dbl Click: Theater View" + "</h3>" + "<br>");
+  var instructions = $("<h3 id='instructions'><span style='color:red;'>Mouseover/Touch: View or Stop Animation </span> <span style='color:blue;'>| Click: Add to Favs </span> <span style='color:green;'>| Dbl Click: Theater View</span></h3><br>");
   $("#instructions-div").append(instructions);
   instructions.hide(0);
   instructions.fadeIn(1000);
@@ -229,6 +260,7 @@ $(document).on("click", ".gif", function() {
   $("#prev-button").on("click", function() {
     if (endingIndex > 5) {
       $("#gifs-view").empty();
+      $("#title-view").empty();
       startingIndex -= 4;
       endingIndex -= 4;
       getGifData(gif);
@@ -249,11 +281,60 @@ $(document).on("click", ".gif", function() {
   $("#next-button").on("click", function() {
     if (startingIndex < 21) {
       $("#gifs-view").empty();
+      $("#title-view").empty();
       startingIndex += 4;
       endingIndex += 4;
       getGifData(gif);
     }
   })
 })
+// view favorites button functionality for displaying favorites area and loading in any saved favorites data from local storage
+$(document).on("click","#fav-button", function() {
 
+  $(".footer").fadeOut(200);
+  $("#container-main").fadeOut(300);
+  var favsContainer = $("<div style='color:white;margin-top:30px;' class='container'><h5><span style='color:green'>SAVE GIF: Right click (or press and hold if on a mobile device).</span><br><br><span style='color:red;'>REMOVE GIF: Double click: Remove from favorites list.</span></h5></div>");
+  var favsView = $("<div id='favs' style='margin-bottom:5px;background-color:transparent;' class='row jumbotron'></div>");
+  $("body").append(favsContainer);
+  favsContainer.append(favsView);
+  
+  // display all favorited gifs
+  for (let i = 0; i < favList.length; i++) {
+    var favDiv = $("<div class='gif-div col-lg-3 col-sm-6 col-md-3 col-xs-12' style='padding:10px;'></div>");
+    var fav = $("<img src='" + favList[i] + "' class='gif-fav' width='100%' height='200px' style='margin-top:5px;margin-bottom:5px;'></img>");
+    fav.attr("data-favNumber", i);
+    favsView.append(favDiv);
+    favDiv.append(fav);
+  }  
 
+  //  add back button
+  var backBtn = $("<button id='back-button' class='col-sm-12 col-xs-12 col-md-12 col-lg-12 btn btn-success'>Back to Search View</button>");
+  favsContainer.append(backBtn);
+
+  backBtn.on("click", function() { 
+    favsContainer.hide();
+    $("#container-main").fadeIn(1000);
+  })
+
+  //  add clear favorites button
+  var clearBtn = $("<button id='clear-button' class='col-sm-12 col-xs-12 col-md-12 col-lg-12 btn btn-success'>Clear All Favorites</button>");
+  favsContainer.append(clearBtn);
+
+  clearBtn.on("click", function() { 
+    favsView.empty();
+    favList = [];
+    localStorage.setItem("favs", JSON.stringify(favList));
+  })
+
+  favsContainer.hide(0);
+  favsContainer.fadeIn(2000);
+
+})
+// clear items from favorites list on double click
+$("body").on("dblclick",".gif-div", function() {
+  var favCount = $(this).attr("favNumber");
+  favList.splice(favCount, 1);
+  localStorage.setItem("favs", JSON.stringify(favList));
+  $(this).hide(200);
+  console.log(favList);
+})
